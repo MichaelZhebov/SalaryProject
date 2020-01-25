5ccdcc91d0468c2c1ceedd99282ab8f95ef62735
MiMiSaR <mzhebov@gmail.com>
2020-01-25 19:02:19 +0200

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <ul class="topnav">
+                    <li><a class="active" href="#news">Salary</a></li>
+                    <li><a href="#news">Contact</a></li>
+                    <li class="right"><a href="#contact">About</a></li>
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default App;
